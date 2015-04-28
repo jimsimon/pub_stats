a1f31400b79a2f4016c3641b591880158463950f
Jim Simon <jim.j.simon@gmail.com>
2015-04-28 16:31:08 -0700

    Fixed issues with UTF8 escaped characters in strings

--- bin/server.dart
+++ bin/server.dart
@@ -7,15 +7,45 @@ import "package:shelf/shelf_io.dart" as io;
 import "package:shelf_route/shelf_route.dart";
 import "package:managed_mongo/managed_mongo.dart";
 import "package:mongo_dart/mongo_dart.dart";
+import "package:pub_client/pub_client.dart";
+import "package:dartson/dartson.dart";
+import "package:dartson/transformers/date_time.dart";
+import "package:logging/logging.dart";
+import "package:dartson/type_transformer.dart";
+import "dart:mirrors";
 
-const Map headers = const {HttpHeaders.CONTENT_TYPE: 'application/json', "Access-Control-Allow-Origin": "*"};
-final PubClient pubClient = new PubClient.forUrl("https://pub.dartlang.org/api");
+final Map _HEADERS = const {"Content-Type": "application/json"};
+
+final Dartson dartson = new Dartson.JSON();
+final PubClient pubClient = new PubClient();
 
 var analysisResult;
 MongoDB mongodb;
 
+Future<Type> _getTwoByteStringType() async {
+  MirrorSystem ms = await currentMirrorSystem();
+  Uri uri = Uri.parse("dart:core");
+  LibraryMirror lm = ms.libraries[uri];
+  Type type;
+  lm.declarations.forEach((Symbol name, DeclarationMirror value){
+    if (MirrorSystem.getName(name).contains("_TwoByteString")) {
+      type = (value as TypeMirror).reflectedType;
+    }
+  });
+  if (type != null) {
+    return type;
+  }
+  throw new Exception("dart.core._TwoByteString type not found");
+}
+
 main() async {
+  Logger.root.level = Level.WARNING;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
   runZoned(() async {
+    dartson.addTransformer(new DateTimeParser(), DateTime);
+    dartson.addTransformer(new TwoByteStringTransformer(), await _getTwoByteStringType());
     var downloadUrl = "https://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.6.5.tgz";
     mongodb = new MongoDB(downloadUrl, workFolder: "mongo");
     await mongodb.start();
@@ -31,13 +61,14 @@ main() async {
     var myRouter = router()
       ..get("/api", (_) => new Response.ok("Hello from API"))
       ..get("/api/packages", (request) => new Response.ok(JSON.encode(analysisResult["packageMap"].keys.toList()
-      ..sort()), headers: headers))
+      ..sort()), headers: _HEADERS))
       ..get("/api/packages/{package}", (request) {
       var report = analysisResult["packageMap"][getPathParameter(request, "package")];
       if (report != null) {
         report["dependents"].sort();
         report["dev_dependents"].sort();
-        return new Response.ok(JSON.encode(report), headers: headers);
+        var json = dartson.encode(report);
+        return new Response.ok(json, headers: _HEADERS);
       } else {
         return new Response.notFound("Package not found");
       }
@@ -53,6 +84,7 @@ main() async {
 
     io.serve(handler, '0.0.0.0', 8080);
   }, onError: (error) async {
+
     if (mongodb != null) {
       await mongodb.stop();
     }
@@ -61,7 +93,9 @@ main() async {
 }
 
 refreshStats() async {
-  var packages = await pubClient.getAllPackages();
+  stdout.write("refreshing packages...");
+  List<Package> packages = await pubClient.getAllPackages();
+  stdout.writeln("done");
 
   Db db = new Db("mongodb://localhost:27017/pubstats");
   await db.open();
@@ -69,52 +103,11 @@ refreshStats() async {
   PackagePersistence packagePersistence = new PackagePersistence(packageDao);
   packages = await packagePersistence.savePackages(packages);
   await db.close();
+  stdout.write("refreshing stats...");
   PackageAnalyzer packageAnalyzer = new PackageAnalyzer(packages);
   analysisResult = packageAnalyzer.runAnalysis();
   new Future.delayed(const Duration(hours: 24), refreshStats);
-}
-
-class PubClient {
-
-  final HttpClient _client = new HttpClient();
-  String baseApiUrl;
-
-  PubClient.forUrl(String baseApiUrl) {
-    this.baseApiUrl = _normalizeUrl(baseApiUrl);
-  }
-
-  _normalizeUrl(String url) {
-    if (url.endsWith("/")) {
-      return url.substring(0, url.length - 1);
-    }
-    return url;
-  }
-
-  getAllPackages() async {
-    var packages = [];
-    var currentPage = 1;
-    var totalPages = 1;
-    while (currentPage <= totalPages) {
-      var response = await getPageOfPackages(currentPage);
-      packages.addAll(response["packages"]);
-      totalPages = response["pages"];
-      currentPage++;
-    }
-    print("${packages.length} packages found");
-    return packages;
-  }
-
-  Future<Map> getPageOfPackages(pageNumber) async {
-    var url = "$baseApiUrl/packages?page=$pageNumber";
-    print("Requesting data from $url");
-    Completer completer = new Completer();
-    HttpClientRequest request = await _client.getUrl(Uri.parse(url));
-    HttpClientResponse response = await request.close();
-    response.transform(UTF8.decoder).transform(JSON.decoder).listen((contents) {
-      completer.complete(contents);
-    });
-    return completer.future;
-  }
+  stdout.writeln("done");
 }
 
 class PackagePersistence {
@@ -122,9 +115,10 @@ class PackagePersistence {
 
   PackagePersistence(PackageDao this.packageDao);
 
-  Future<List> savePackages(List packages) async {
+  Future<List> savePackages(List<Package> packages) async {
     await packageDao.insertPackages(packages);
-    return await packageDao.getPackages();
+    var dbPackages = await packageDao.getPackages();
+    return dbPackages;
   }
 }
 
@@ -133,34 +127,35 @@ class PackageDao {
 
   PackageDao(DbCollection this.collection);
 
-  Future insertPackages(List packages) async {
-    return collection.insertAll(packages);
+  Future insertPackages(List<Package> packages) async {
+    List<Map> data = dartson.serialize(packages);
+    collection.insertAll(data);
   }
 
-  Future<List<Map>> getPackages() async {
-    return collection.find().toList();
+  Future<List<Package>> getPackages() async {
+    List<Map> data = await collection.find().toList();
+    return dartson.map(data, new Package(), true);
   }
-
 }
 
 class PackageAnalyzer {
-  List _packages = [];
+  List<Package> _packages = [];
   var packageMap = {};
 
-  PackageAnalyzer(List this._packages);
+  PackageAnalyzer(List<Package> this._packages);
 
   runAnalysis() {
     print("Running analysis for ${_packages.length} packages");
     _packages.forEach((package) {
-      var name = package["name"];
+      var name = package.name;
 
       packageMap.putIfAbsent(name, () => {});
       packageMap[name]["package"] = package;
       packageMap[name].putIfAbsent("dependents", () => []);
       packageMap[name].putIfAbsent("dev_dependents", () => []);
 
-      var pubspec = package["latest"]["pubspec"];
-      Map dependencies = pubspec["dependencies"];
+      var pubspec = package.latest.pubspec;
+      Map dependencies = pubspec.dependencies;
       if (dependencies != null) {
         dependencies.forEach((key, value) {
           packageMap.putIfAbsent(key, () => {});
@@ -169,7 +164,7 @@ class PackageAnalyzer {
         });
       }
 
-      var devDependencies = pubspec["dev_dependencies"];
+      var devDependencies = pubspec.dev_dependencies;
       if (devDependencies != null) {
         devDependencies.forEach((key, value) {
           packageMap.putIfAbsent(key, () => {});
@@ -183,5 +178,18 @@ class PackageAnalyzer {
     return {
       "packageMap": packageMap
     };
+  }
+}
+
+class TwoByteStringTransformer extends TypeTransformer {
+
+  @override
+  decode(value) {
+    return value as String;
+  }
+
+  @override
+  encode(value) {
+    return value as String;
   }
 }
